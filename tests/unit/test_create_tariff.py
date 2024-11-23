54527b89e0338e1059868c148a51eb323668518d
Daniil Bozhko <voc.offacc@gmail.com>
2024-11-23 16:26:33 +0600

    test: add db check in unit test of tariff creation

--- tests/unit/test_create_tariff.py
+++ tests/unit/test_create_tariff.py
@@ -1,8 +1,10 @@
 import datetime
 import pytest
+from sqlalchemy import select
 
 from src.tariff.service import create_tariff_plans
 from src.tariff.schemas import DailyTariffsRequest
+from src.tariff.models import MaterialRate
 
 
 @pytest.mark.asyncio
@@ -16,5 +18,9 @@ async def test_create_tariffs(session):
         tariffs=DailyTariffsRequest.model_validate(data), db=session
     )
 
+    scres = await session.scalars(select(MaterialRate))
+    items = scres.all()
     assert res[0].relevance_date == datetime.date(2020, 1, 1)
     assert res[1].relevance_date == datetime.date(2020, 1, 2)
+    assert items[0].material_type == "Glass"
+    assert items[1].rate == 0.3
