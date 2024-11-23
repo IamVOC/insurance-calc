a6fb1d947d026e9d9fea6736a27de25c230a33db
Daniil Bozhko <voc.offacc@gmail.com>
2024-11-23 22:22:12 +0600

    feat: add route for insurance calculation

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from src.config import settings
 from src.tariff.router import router as tariff_router
+from src.insurance.router import router as insurance_router
 
 
 app = FastAPI(root_path="/api/v1")
@@ -10,3 +11,4 @@ if settings.ENVIRONMENT not in ("local", "production"):
     app.openapi_url = None
 
 app.include_router(tariff_router)
+app.include_router(insurance_router)
