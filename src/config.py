e70ac76117d576059f4ab5431ce40a43c1362cf6
Daniil Bozhko <voc.offacc@gmail.com>
2024-11-23 16:20:13 +0600

    fix: add standard env file for config

--- src/config.py
+++ src/config.py
@@ -20,7 +20,9 @@ class DBConfig(BaseSettings):
 
 
 class Config(BaseSettings):
-    model_config = SettingsConfigDict(env_nested_delimiter="_")
+    model_config = SettingsConfigDict(
+        env_nested_delimiter="_", env_file=".env", env_file_encoding="utf-8"
+    )
 
     ENVIRONMENT: Environment = Environment.production
 
